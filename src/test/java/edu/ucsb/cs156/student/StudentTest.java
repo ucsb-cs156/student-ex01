f497c826f5d8c69370d2e1c5fa5cc80b62143139
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-01-05 11:46:52 -0800

    pc - update for Java 17, Junit 5, newer versions of Jacoco and Pitest

--- src/test/java/edu/ucsb/cs156/student/StudentTest.java
+++ src/test/java/edu/ucsb/cs156/student/StudentTest.java
@@ -1,7 +1,7 @@
 package edu.ucsb.cs156.student;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class StudentTest {
 
@@ -24,3 +24,4 @@ public class StudentTest {
     }
 
 }
+
