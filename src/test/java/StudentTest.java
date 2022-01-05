58d9d3ea2e85efdb922ad3e5a57587c58e00a03c
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-01-05 10:44:10 -0800

    pc - update for Java 17 and JUnit 5

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,5 +1,6 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentTest {
 
